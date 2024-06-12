21c6ef3e8dce09f2b3c3dc0d3de47014bf9326ef
vcc2005 <00001100199743sp@al.educacao.sp.gov.br>
2024-06-12 08:18:40 -0300

    adicionanado arquivos js e css

---
+++ sket.js
@@ -0,0 +1,32 @@
+let cor;
+let posicaoHorizontal; // horizontal
+let posicaoVertical; // vertical
+
+function setup() {
+  createCanvas(400, 400);
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+ 
+  posicaoHorizontal = [0, 0, 0];
+  posicaoVertical = [random(height), random(height), random(height)];
+}
+
+function draw() {
+ 
+  fill(cor);
+ 
+  for(let contador in posicaoHorizontal) {
+    circle(posicaoHorizontal[contador], posicaoVertical[contador], 50);    
+    posicaoHorizontal[contador]+= random(0,3);
+    posicaoVertical[contador]+= random(-3,3);
+   
+    if(posicaoHorizontal[contador] >= width){
+      posicaoHorizontal[contador] = 0;
+      posicaoVertical[contador] = random(height);
+    }
+  }
+ 
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
